bc51a0044bae0357f1415a5f58291d27af8bcfc6
Jorge Cervantes <jcdev1825@gmail.com>
2023-11-27 23:20:32 -0600

    feat(script-01): get word to search by input

--- script_01.py
+++ script_01.py
@@ -1,3 +1,6 @@
+print('What word that you need search?')
+word_to_search = input()
 with open('input_01.txt','r') as input_file:
+    print('Word to search: ', word_to_search)
     for text_line in input_file:
         print(text_line.strip())
