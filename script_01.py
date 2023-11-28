96aa65293dc361c7802eba6990fbd8a3b37417ca
Jorge Cervantes <jcdev1825@gmail.com>
2023-11-27 23:39:17 -0600

    feat(script-01): remove special chars by regex

--- script_01.py
+++ script_01.py
@@ -1,6 +1,13 @@
-print('What word that you need search?')
-word_to_search = input()
+import re
+
+def remove_special_chars(string):
+    return re.sub(r'[^a-zA-Z0-9]', '', string)
+
+print('What word do you need search?')
+word_to_search = input().strip()
 with open('input_01.txt','r') as input_file:
     print('Word to search: ', word_to_search)
     for text_line in input_file:
-        print(text_line.strip())
+        for word in text_line.strip().split():
+            word_sanitized = remove_special_chars(word)
+            print(word_sanitized)
