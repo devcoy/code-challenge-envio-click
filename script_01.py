d545a6e60e81d47fc79c076bc49863d45a488d62
Jorge Cervantes <jcdev1825@gmail.com>
2023-11-28 00:45:02 -0600

    refactor(script-01): close file

--- script_01.py
+++ script_01.py
@@ -5,13 +5,15 @@ def remove_special_chars(string):
 
 print('What word do you need search?')
 word_to_search = input().strip()
+count = 0;
+
 with open('input_01.txt','r') as input_file:
     print('Word to search: ', word_to_search)
-    count = 0;
     for text_line in input_file:
         for word in text_line.strip().split():
             word_sanitized = remove_special_chars(word)
             if word_sanitized == word_to_search:
                 count+= 1
-    input_file.close()
-    print(f'The word has {count} matched.')
+
+input_file.close()
+print(f'The word has {count} matched.')
