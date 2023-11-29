e7e9fb5dca8839c2bbe81f7c578d6d8a4a84538a
Jorge Cervantes <jcdev1825@gmail.com>
2023-11-29 01:25:49 -0600

    feat(script-02): read every package and condition

--- script_02.py
+++ script_02.py
@@ -3,12 +3,31 @@ import json
 class Package:
     packages = []
     criteria = []
+    condition_property_name = ''
+    condition_assignment_operator = ''
+    condition_value = 0
+    packages_by_criteria = []
 
     def __init__(self, file_path, criteria):
         with open(file_path, 'r') as input_file:
             self.packages = json.load(input_file)
         input_file.close()
         self.criteria = criteria
+
+    def set_condition(self, condition):
+        self.condition_property_name = condition[0];
+        self.condition_assignment_operator = condition[1]
+        self.condition_value = condition[2]
+
+    def get_packages_by_condition(self):
+        for package in self.packages:
+            for condition in self.criteria:
+                self.set_condition(condition)
+
+    def get_packages_ordered(self):
+        self.get_packages_by_condition()
+        print(self.packages_by_criteria)
+        return []
 
 
 file_path = 'input_02.json'
@@ -22,3 +41,4 @@ criteria = [
 ]
 
 package = Package(file_path, criteria)
+packages_ordered = package.get_packages_ordered()
