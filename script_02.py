41f92faa106bd7ae55f03b50b628063f362aa13b
Jorge Cervantes <jcdev1825@gmail.com>
2023-11-29 01:19:37 -0600

    refactor(script-02): open input file and read it content using a class

--- script_02.py
+++ script_02.py
@@ -1,9 +1,24 @@
 import json
 
+class Package:
+    packages = []
+    criteria = []
+
+    def __init__(self, file_path, criteria):
+        with open(file_path, 'r') as input_file:
+            self.packages = json.load(input_file)
+        input_file.close()
+        self.criteria = criteria
+
+
 file_path = 'input_02.json'
+# criteria = [
+#     ('weight', '=', 3)
+# ]
 
-with open(file_path, 'r') as input_file:
-    data_array = json.load(input_file)
+criteria = [
+    ('width', '>=', 2),
+    ('length', '<=', 9),
+]
 
-input_file.close()
-print("Array from file:", data_array)
+package = Package(file_path, criteria)
